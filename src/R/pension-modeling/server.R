29cee8cd8874ca6869e15ab49154574b375a8583
Paul Wilczewski <wilczewski@gmail.com>
2015-09-15 19:49:44 -0500

    Changed server.R

--- src/R/pension-modeling/server.R
+++ src/R/pension-modeling/server.R
@@ -2,7 +2,7 @@ source('source_functions.R')
 
 # Hard-coded values for now
 start_year = 2014
-maxage = 90
+maxage = 105
 starting_wealth = c(56789460)
 
 server <- shinyServer(function(input,output,clientData,session) {
@@ -136,7 +136,7 @@ server <- shinyServer(function(input,output,clientData,session) {
   fundingRatio <- reactive({
     tier1 = forecast_population(curr_actives_tier1,curr_beneficiaries,input$npers,tier=1)
     tier2 = forecast_population(curr_actives_tier2,curr_beneficiaries,input$npers,tier=2)
-    no_replacement_pop = list((tier1[[1]]+tier2[[1]])/2,(tier1[[2]]+tier2[[2]])/2)
+    no_replacement_pop = list(tier1[[1]]+tier2[[1]],(tier1[[2]]+tier2[[2]])/2)
     calculate_funding_ratio(starting_wealth[1],no_replacement_pop,avg_benefits_forecast(),input$ror)
   })
   
@@ -147,7 +147,7 @@ server <- shinyServer(function(input,output,clientData,session) {
   
   # Calculate contribution target based on the funding ratio
   output$contributionTarget <- renderText({
-    contributions_needed = (input$tfr-fundingRatio())/100*starting_wealth[1]
+    contributions_needed = starting_wealth[1] / fundingRatio() * input$tfr
     if (contributions_needed < 0) {
       contributions_needed = 0
     }
